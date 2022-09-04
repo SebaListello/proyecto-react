1c124102c15fc4ca30b94013de13ea5d23806848
SebaListello <sebastianlistello98@gmail.com>
2022-09-04 20:09:27 -0300

    Instalacion de Libreria CSS
    Instalacion de Tailwind en el proyecto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-    </div>
-  );
+    <h1 className="text-3xl font-bold underline bg-red-800">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
