efb467f0844d9c7cb034e59fe48c0469fc8df36a
SebaListello <sebastianlistello98@gmail.com>
2022-09-06 20:47:58 -0300

    Confirmando prueba

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline bg-red-800">
-      Hello world! probando
+      Hello world! probando, 123 123, si, anda
     </h1>
   )
 }
