73f444826151302bfdd8e9eaca396a72b8805b2a
SebaListello <sebastianlistello98@gmail.com>
2022-09-08 21:22:10 -0300

    Creacion Estructura NavBar

---
+++ src/Components/NavBarFolder/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const NavBar = () => {
+  return ( 
+    <nav className='flex flex-row w-full h-[7%] bg-red-500 justify-between items-center'>
+      <button className='bg-blue-500'>ICONO MENU
+
+      </button>
+      <div className='bg-yellow-500'>LOGO
+      </div>
+      <button className='bg-orange-500'>CARRITO
+      </button>
+    </nav>
+// BOTON DE MENÚ MOVIL
+// LOGO
+// BOTON DE CARRITO
+// LISTA OPCIONES DESKTOP
+// LISTA OPCIONES MOVIL
+  )
+}
+
+export default NavBar
