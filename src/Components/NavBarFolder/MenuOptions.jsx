411b1eefeba939eacba296a71464fc8c1f6033bb
SebaListello <sebastianlistello98@gmail.com>
2022-09-12 20:06:45 -0300

    Modularizacion de componentes en NavBar
    dividi partes de codigo en distintos jsx asi me quedan componentes mas chicos y mas limpia la pagina de la navbar

--- src/Components/NavBarFolder/MenuOptions.jsx
+++ src/Components/NavBarFolder/MenuOptions.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MenuOptions = () => {
   return (
-    <div className='absolute md:hidden w-full bg-orange-500 top-[10%] flex justify-center items-center py-[3%]'>
+    <div className='absolute w-full bg-orange-500 top-[10%] flex justify-center items-center py-[3%]'>
         <ul className='flex flex-col leading-8 text-[0.9rem] font-inter font-normal text-white tracking-widest items-center'>
             <li><a href="#">Inicio</a></li>
             <li><a href="#">Hombres</a></li>
