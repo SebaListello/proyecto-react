05836617253018bcf5d0e342ade0d54f426adc60
SebaListello <sebastianlistello98@gmail.com>
2022-09-14 12:31:25 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import NavBar from "./Components/NavBarFolder/NavBar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-800">
-      Hello world! probando, 123 123, si, anda
-    </h1>
+    <div className="h-screen">
+      <NavBar></NavBar>
+    </div>
   )
 }
