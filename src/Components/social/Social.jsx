aae8b87f7455028d8dc263275d8e8ab9978a9ef0
Julian Rebolledo <rebolledojulian96@gmail.com>
2022-09-27 19:47:44 -0300

    Corregido el responsive de la seccion redes sociales

--- src/Components/social/Social.jsx
+++ src/Components/social/Social.jsx
@@ -1,8 +1,8 @@
-import { FaFacebook, FaInstagram, FaTwitter, FaFacebookMessenger, FaWhatsapp} from 'react-icons/fa'
+import { FaFacebook, FaInstagram, FaTwitter, FaFacebookMessenger, FaWhatsapp, FaTelegram} from 'react-icons/fa'
 
 export const Social = () => {
   return (
-    <div className="social__container h-[35vh] bg-orange-500 flex flex-col items-center justify-center mt-10">
+    <div className="social__container h-[35vh] bg-orange-500 flex flex-col items-center justify-center mt-10 px-2 sm:px-0">
         <h2 className='uppercase font-bold text-2xl tracking-wider'>Seguinos</h2>
         <div className="social__container-group flex items-center gap-6 mt-5">
             <div className="social__container-social hover:text-white transition duration-100 ease-in-out hover:scale-[1.15]">
@@ -28,6 +28,11 @@ export const Social = () => {
             <div className="social__container-social hover:text-white transition duration-100 ease-in-out hover:scale-[1.15]">
                 <a href="#">
                     <FaWhatsapp size={34}/>
+                </a>
+            </div>
+            <div className="social__container-social hover:text-white transition duration-100 ease-in-out hover:scale-[1.15]">
+                <a href="#">
+                    <FaTelegram size={34}/>
                 </a>
             </div>
         </div>
