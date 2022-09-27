4c1e5f9aaca4ba41fe6e62ae250f2d605d597957
Julian Rebolledo <rebolledojulian96@gmail.com>
2022-09-26 23:16:19 -0300

    Subido componente moreBrands

---
+++ src/assets/moreBrands/index.js
@@ -0,0 +1,7 @@
+import morebrands_1 from './morebrands_1.svg';
+import morebrands_2 from './morebrands_2.svg';
+import morebrands_3 from './morebrands_3.svg';
+import morebrands_4 from './morebrands_4.svg';
+
+
+export { morebrands_1, morebrands_2, morebrands_3, morebrands_4}
